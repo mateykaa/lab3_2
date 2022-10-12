12b619a81994094851611a54ee65c700bf5e9ab1
mateykaa <81904136+mateykaa@users.noreply.github.com>
2022-10-13 02:43:56 +0300

    Add files via upload

--- lab3_2.cpp
+++ lab3_2.cpp
@@ -28,9 +28,52 @@ void task2(int n,int *array)
 }
 
 // Сортировка по сумме цифр, стоящих на четных местах.
-void task3()
+void task3(int n, int* array)
 {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            int x1 = array[j],kx1=0,chetx1=0,nechetx1=0,summx1;
+            int x2 = array[j + 1], kx2 = 0, chetx2 = 0, nechetx2 = 0, summx2;
+            while (x1) {
+                kx1++;
+                if (!(kx1 % 2)) {
+                    chetx1 += x1 % 10;
+                }
+                else {
+                    nechetx1 += x1 % 10;
+                }
+                x1 /= 10;
+            }
+            if (!(kx1 % 2)) {
+                summx1 = nechetx1;
+            }
+            else {
+                summx1 = chetx1;
+            }
 
+            while (x2) {
+                kx2++;
+                if (!(kx2 % 2)) {
+                    chetx2 += x2 % 10;
+                }
+                else {
+                    nechetx2 += x2 % 10;
+                }
+                x2 /= 10;
+            }
+            if (!(kx2 % 2)) {
+                summx2 = nechetx2;
+            }
+            else {
+                summx2 = chetx2;
+            }
+            if (summx1 > summx2) {
+                int new_peremen = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = new_peremen;
+            }
+        }
+    }
 }
 
 // Отсортировать массив вначале по возрастанию последней цифры, а при совпадении последних цифр - по убыванию.
